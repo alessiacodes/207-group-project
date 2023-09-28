a52a4dce75b7a967052865503e8156248039fa18
Alessia <alessia.ruberto04@gmail.com>
2023-09-28 13:56:41 -0400

    Api call successful, need to process JSON response

--- src/main/java/Testing_api.java
+++ src/main/java/Testing_api.java
@@ -1,2 +1,39 @@
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class Testing_api {
+    private static final String APP_ID  = "64984032";
+    private static final String APP_KEY  = "47ecdbab5b1aa48bcbd2c622f83c8006";
+
+    public static void main(String[] args) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        try {
+            // Build correct URL with appropriate parameters, in this case I'm using the API to find the nutritional
+            // content of rice.
+            HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.edamam.com/api/nutrition-data").newBuilder();
+            urlBuilder.addQueryParameter("app_id", APP_ID);
+            urlBuilder.addQueryParameter("app_key", APP_KEY);
+            urlBuilder.addQueryParameter("ingr", "rice (1 cup)");
+            String apiURL = urlBuilder.build().toString();
+
+            // Making the actual request
+            Request request = new Request.Builder()
+                    .url(apiURL)
+                    .build();
+
+            // Getting the response
+            Response response = client.newCall(request).execute();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
