0b6b3a8916927f40e2942000597fadb520ab195e
Rachel Kim <rachelkim4321@gmail.com>
2023-10-23 16:48:34 -0400

    changes not pushed from recipe file; attempt 2

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -1,2 +1,37 @@
+package entity;
+
 public class Recipe {
+    private final String recipeName;
+
+    private final Integer kCal;
+
+    private final LinkedHashMap<Food, Integer> ingredients;
+
+    Recipe(String recipeName, Integer kCal, LinkedHashMap<Food, Integer> ingredients) {
+        this.recipeName = recipeName;
+        this.kCal = kCal;
+        this.ingredients = ingredients;
+    }
+
+    public double getTotalCalories() {
+        ...;
+    }
+
+    public double getTotalNutrients() {
+        ...;
+    }
+
+    public void addFood() {
+        ...;
+    }
+
+    @Override
+    public String getRecipeName() {return recipeName;}
+
+    @Override
+    public Integer getkCal() {return kCal;}
+
+    @Override
+    public LinkedHashMap<Food, Integer> getIngredients() {return ingredients;}
+
 }
