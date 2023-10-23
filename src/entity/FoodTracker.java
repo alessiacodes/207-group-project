5dffcf59f3f6c33c3a59de2b8d3c70103034bc3c
Alessia <alessia.ruberto04@gmail.com>
2023-10-23 16:45:04 -0400

    skeleton for food diary class

---
+++ src/entity/FoodTracker.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class FoodTracker {
+    private List<Food> foodDiary;
+    private List<Double> waterDiary;
+
+    public double getTotalCalories(){
+
+    }
+
+    public double getTotalNutrition(str nutrient){
+
+    }
+
+    public void addFood(Food food){
+        foodDiary.add(food);
+    }
+
+    public void removeFood(Food food){
+        foodDiary.remove(food);
+    }
+
+}
