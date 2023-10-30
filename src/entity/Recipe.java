82c60bffe479e51e2a6ac62954dc989f94cfcf71
Rachel Kim <rachelkim4321@gmail.com>
2023-10-30 16:11:34 -0400

    made basic recipe entity class

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -1,5 +1,9 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 public class Recipe {
     private final String recipeName;
 
@@ -14,17 +18,39 @@ public class Recipe {
     }
 
     public double getTotalCalories() {
-        ...;
+        double calories = 0;
+        Set<Food> ingre = ingredients.keySet();
+        for (Food food : ingre) {
+            calories += ingredients.get(food) * food.getCalories();
+        }
+        return calories;
     }
 
-    public double getTotalNutrients() {
-        ...;
+    public LinkedHashMap<String, Double> getTotalNutrients() {
+        Set<Food> ingre = ingredients.keySet();
+        LinkedHashMap<String, Double> total = new LinkedHashMap<String, Double>();
+
+        for (Food food : ingre) {
+            LinkedHashMap<String, Double> hashNutrients = food.getNutritionalValues();
+            Set<String> nutrients = hashNutrients.keySet();
+            for (String nutrient : nutrients) {
+                if (total.containsKey(nutrient)) {
+                    total.put(nutrient, (total.get(nutrient) + hashNutrients.get(nutrient)));
+                }
+                else {
+                    total.put(nutrient, hashNutrients.get(nutrient));
+                }
+            }
+        }
+
+        return total;
     }
 
-    public void addFood() {
-        ...;
+    public void addFood(Food food, Integer amount) {
+        ingredients.put(food, amount);
     }
 
+    /*
     @Override
     public String getRecipeName() {return recipeName;}
 
@@ -33,5 +59,6 @@ public class Recipe {
 
     @Override
     public LinkedHashMap<Food, Integer> getIngredients() {return ingredients;}
+    */
 
 }
