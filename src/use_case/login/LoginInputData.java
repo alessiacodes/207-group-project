064f462e782063779d661826f47fbd9cd071e45c
manaljav <madridmanaljav@gmail.com>
2023-10-30 16:11:39 -0400

    Modified LoginInputData.java

--- src/use_case/login/LoginInputData.java
+++ src/use_case/login/LoginInputData.java
@@ -1,1 +1,13 @@
+public class LoginInputData {
 
+    private String username;
+    private String password;
+
+    public LoginInputData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+}
