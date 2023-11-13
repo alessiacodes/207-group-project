ae1b67b2b2679b9e6d778894f92c5c4db3c39af7
Alessia <alessia.ruberto04@gmail.com>
2023-11-13 16:12:37 -0500

    Added link to my 207 notes

--- src/use_case/signup/SignupController.java
+++ src/use_case/signup/SignupController.java
@@ -1,4 +1,24 @@
 package use_case.signup;
 
+import java.util.ArrayList;
+
 public class SignupController {
+    final SignupInputBoundary interactor;
+    public SignupController(SignupInputBoundary interactor){
+        this.interactor = interactor;
+    }
+    
+    public void execute(String username,
+                        String password,
+                        String name,
+                        String gender,
+                        int age,
+                        Double height,
+                        ArrayList<String> dietaryRestrictions,
+                        Double weight){
+        SignupInputData inputData = new SignupInputData(username, password, name, gender, age, height, dietaryRestrictions,weight);
+        interactor.execute(inputData);
+    }
+
+
 }
