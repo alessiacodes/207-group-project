641ff4a612aa1765bbece9eb9922ce0cbf88a70d
Alessia <alessia.ruberto04@gmail.com>
2023-11-13 16:14:03 -0500

    Fixed signup controller

--- src/use_case/signup/SignupController.java
+++ src/use_case/signup/SignupController.java
@@ -16,9 +16,9 @@ public class SignupController {
                         Double height,
                         ArrayList<String> dietaryRestrictions,
                         Double weight){
+
         SignupInputData inputData = new SignupInputData(username, password, name, gender, age, height, dietaryRestrictions,weight);
         interactor.execute(inputData);
     }
-
 
 }
