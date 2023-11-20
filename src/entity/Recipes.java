7e797d688accf0642bc75c15477d8e8c6f3dc744
Rachel Kim <rachelkim4321@gmail.com>
2023-11-20 15:51:35 -0500

    edited recipe entity class and interface

--- src/entity/Recipes.java
+++ src/entity/Recipes.java
@@ -1,2 +1,14 @@
-package entity;public interface Recipes {
+package entity;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+
+public interface Recipes {
+    String getRecipeName();
+
+    LinkedHashMap<Food, Integer> getIngredients();
+
+    Integer getkCal();
+
+    LinkedHashMap<String, Double> getTotalNutrients();
 }
