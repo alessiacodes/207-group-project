36d2e81b7261e400a30605c28c0a72bbf10cd6ca
Alessia <128920588+alessiacodes@users.noreply.github.com>
2023-11-20 16:32:40 -0500

    Update EdamamApiAccess.java

--- src/data_access/EdamamApiAccess.java
+++ src/data_access/EdamamApiAccess.java
@@ -6,8 +6,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.IOException;
 class EdamamApiAccess {
-    private static final String APP_ID  = "64984032";
-    private static final String APP_KEY  = "47ecdbab5b1aa48bcbd2c622f83c8006";
+    private static final String APP_ID  = "64984032"; //this is for food lookup
+    private static final String APP_KEY  = "47ecdbab5b1aa48bcbd2c622f83c8006"; //this is for food lookup
+
+    //IMPORTANT: THESE API KEYS ARE FOR RECIPE API
+    //Application ID: cd905d5f
+    //Key: 19dbdd026906aa90c7a5ca301942a30d	
 
 //    public static tuple getFoodData(String food, String Quantity){
 //
