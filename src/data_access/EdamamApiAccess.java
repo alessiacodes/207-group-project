f1d4194f5d7e1d042d0ad326b7f6748b72204adf
Rachel Kim <rachelkim4321@gmail.com>
2023-11-23 15:52:14 -0500

    minor edit to resolve changes

--- src/data_access/EdamamApiAccess.java
+++ src/data_access/EdamamApiAccess.java
@@ -10,8 +10,12 @@ import java.util.LinkedHashMap;
 import entity.Food;
 
 class EdamamApiAccess {
-    private static final String APP_ID = "64984032";
-    private static final String APP_KEY = "47ecdbab5b1aa48bcbd2c622f83c8006";
+    private static final String APP_ID = "64984032"; //this is for food lookup
+    private static final String APP_KEY = "47ecdbab5b1aa48bcbd2c622f83c8006"; //this is for food lookup
+
+    //IMPORTANT: THESE API KEYS ARE FOR RECIPE API
+    //Application ID: cd905d5f
+    //Key: 19dbdd026906aa90c7a5ca301942a30d
 
     public static JSONObject apiHelper(Food identifier) {
         String foodName = identifier.getName();
