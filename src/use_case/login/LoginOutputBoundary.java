94ee500bb2d8986dd48e015c53a68d9427842c1a
Rachel Kim <rachelkim4321@gmail.com>
2023-11-30 16:06:45 -0500

    fixed more errors; again changed to interface where necessary

--- src/use_case/login/LoginOutputBoundary.java
+++ src/use_case/login/LoginOutputBoundary.java
@@ -1,7 +1,7 @@
 package use_case.login;
 
 public interface LoginOutputBoundary {
-    void prepareSuccessView(LoginOutpuData user);
+    void prepareSuccessView(LoginOutputData user);
 
     void prepareFailView(String error);
     }
