10fb22b8844857e6e6817e6f3f3a8ea089368e98
Natalia Tabja <nataliatabjab@gmail.com>
2023-12-01 15:13:35 -0500

    Implemented TrackViewModel

--- src/use_case/track/TrackViewModel.java
+++ src/use_case/track/TrackViewModel.java
@@ -1,2 +1,34 @@
-package use_case.track;public class TrackViewModel {
+package use_case.track;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class TrackViewModel {
+
+    private TrackState state = new TrackState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    private String viewName;
+
+    public TrackViewModel(String viewName) {
+        this.viewName = viewName;
+    }
+    public String getViewName() {
+        return this.viewName;
+    }
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public TrackState getState() {
+        return state;
+    }
+    public void setState(TrackState state) {
+        this.state = state;
+    }
+
 }
