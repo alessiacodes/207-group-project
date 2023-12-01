6b882fe39f3539f274aabebe9fa7135fe9aa7db0
pergolav <victoria.pergola@mail.utoronto.ca>
2023-11-30 20:16:07 -0500

    completed recommend interactor and fixed variable types throughout to reflect api call output.

--- src/use_case/recommend/RecommendDataAccessInterface.java
+++ src/use_case/recommend/RecommendDataAccessInterface.java
@@ -1,10 +1,11 @@
 package use_case.recommend;
 
 import java.util.ArrayList;
-import entity.User; //may need to use for preferences ????
+
+import entity.Recommend;
 
 public interface RecommendDataAccessInterface {
     //TODO might need more functions here
-    ArrayList<String> getMealList(String diet, String health, String mealType);
+    String getRecommendLink(Recommend identifier);
 
 }
