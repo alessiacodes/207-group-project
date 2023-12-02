d5fd79b82ef910943b46d4989b081d7292caf8d8
Rachel Kim <rachelkim4321@gmail.com>
2023-12-02 13:38:41 -0500

    started making tests for use cases; unfinished

--- test/recipe/testRecipe.java
+++ test/recipe/testRecipe.java
@@ -1,2 +1,35 @@
-package recipe;public class testRecipe {
+package recipe;
+
+import use_case.recipe.RecipeDataAccessInterface;
+import entity.Food;
+import entity.Recipe;
+import use_case.recipe.*;
+import use_case.food.*;
+import view.MainView;
+import entity.BasicUser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class testRecipe {
+    public static void main(String[] args) {
+        testWithoutUI();
+    }
+
+    public void testWithoutUI(){
+        FakeDAO fakeDAO = new FakeDAO();
+    }
+}
+
+class FakeDAO implements RecipeDataAccessInterface {
+    @Override
+    public Integer getCalories(Food identifier) {
+        return identifier.getCalories();
+    }
+
+    @Override
+    public HashMap<String, Double> getTotalNutrients(Food identifier) {
+        return identifier.getTotalNutrients;
+    }
 }
