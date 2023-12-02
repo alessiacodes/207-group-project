1f4164889493fc0c8756ce7dac6bcd2197c586c7
MJ Munkhbayar <78574063+Manal-jpg@users.noreply.github.com>
2023-12-01 22:11:14 -0500

    Finished implementing Food Use Case files, edited minor logical errors within the use case

--- src/use_case/food/FoodOutputData.java
+++ src/use_case/food/FoodOutputData.java
@@ -11,19 +11,33 @@ public class FoodOutputData {
 
     private final HashMap<String, Double> nutritionalValues;
 
+    private Float quantity;
+
     private final Integer kCal;
 
     public FoodOutputData(String foodName,
+                          Float quantity,
                           HashMap<String, Double> nutritionalValues,
                           Integer kCal)
     {
         this.foodName = foodName;
+        this.quantity = quantity;
         this.nutritionalValues = nutritionalValues;
         this.kCal = kCal;
     }
 
-    public String getFoodName() {return this.foodName;}
-    public HashMap<String, Double> getNutritionalValues() {return this.nutritionalValues;}
+    public String getFoodName() {
+        return this.foodName;
+    }
 
-    public Integer getKcal() {return this.kCal;};
+    public Float getQuantity() {
+        return this.quantity;
+    }
+    public HashMap<String, Double> getNutritionalValues() {
+        return this.nutritionalValues;
+    }
+
+    public Integer getKcal() {
+        return this.kCal;
+    };
 }
