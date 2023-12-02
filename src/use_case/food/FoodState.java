ae13f73524226f37000ca72e4c020e989c0e7f8f
MJ Munkhbayar <78574063+Manal-jpg@users.noreply.github.com>
2023-12-01 20:29:59 -0500

    Added FoodState

---
+++ src/use_case/food/FoodState.java
@@ -0,0 +1,47 @@
+package use_case.food;
+
+import java.util.HashMap;
+
+public class FoodState {
+
+    private String foodName;
+
+    private Integer kCal;
+
+    private HashMap<String, Double> nutritionalValues;
+
+    public FoodState(FoodState foodState) {
+        foodName = foodState.foodName;
+        nutritionalValues = foodState.nutritionalValues;
+        kCal = foodState.kCal;
+    }
+
+    // Getters for the Food State
+    public String getFoodName() {
+        return foodName;
+    };
+
+    public Integer getkCal() {
+        return kCal;
+    }
+
+    public HashMap<String, Double> getNutritionalValues() {
+        return nutritionalValues;
+    };
+
+
+    //    Setters for the Food State
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public void setkCal(Integer kCal) {
+        this.kCal = kCal;
+    }
+
+    public void setNutritionalValues(HashMap<String, Double> nutritionalValues) {
+        this.nutritionalValues = nutritionalValues;
+    }
+
+}
