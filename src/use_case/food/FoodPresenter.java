f752cf7c3e7e7fa067f9aa802c3fe681bd76c89c
MJ Munkhbayar <78574063+Manal-jpg@users.noreply.github.com>
2023-12-01 21:20:04 -0500

    Added target fodler to gitignore.

--- src/use_case/food/FoodPresenter.java
+++ src/use_case/food/FoodPresenter.java
@@ -1,5 +1,22 @@
 package use_case.food;
 
-public class FoodPresenter {
-    // will come back
+public class FoodPresenter implements FoodOutputBoundary{
+
+    private final FoodViewModel foodViewModel;
+
+    public FoodPresenter(FoodViewModel foodViewmodel) {
+        this.foodViewModel = foodViewmodel;
+    }
+
+    @Override
+    public void prepareSuccessView(FoodOutputData outputData) {
+//        Not sure if I need a success message
+//        String successMesssage = "";
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
