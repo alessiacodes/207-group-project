9c503b1268d704e38dd23be4d1c852c2cc84e72a
Natalia Tabja <nataliatabjab@gmail.com>
2023-12-03 11:48:26 -0500

    Updates to the track use case, made sure to update the nutritional values of the tracker based on those of the food that is being added

--- src/use_case/track/TrackController.java
+++ src/use_case/track/TrackController.java
@@ -2,19 +2,21 @@ package use_case.track;
 
 import entity.Food;
 import entity.Tracker;
+import use_case.track.TrackInputBoundary;
+import use_case.track.TrackInputData;
 
 public class TrackController {
 
     private final TrackInputBoundary trackInteractor;
+    private final Tracker tracker;
 
-    public TrackController(TrackInputBoundary trackInteractor) {
+    public TrackController(TrackInputBoundary trackInteractor, Tracker tracker) {
         this.trackInteractor = trackInteractor;
+        this.tracker = tracker;
     }
 
     public void execute(String foodName, String quantity) {
-
         // Gather input data
-        Tracker tracker = new Tracker();
         Food food = new Food(foodName, Float.parseFloat(quantity));
         TrackInputData input = new TrackInputData(food, tracker);
 
--- src/use_case/track/TrackOutputData.java
+++ src/use_case/track/TrackOutputData.java
@@ -1,15 +1,19 @@
 package use_case.track;
 
 import entity.Food;
+import entity.Tracker;
 
 public class TrackOutputData {
 
     private final Food food;
-    private boolean useCaseFailed;
 
-    public TrackOutputData(Food food) {
+    private Tracker tracker;
+
+    public TrackOutputData(Food food, Tracker tracker) {
         this.food = food;
     }
 
     public Food getFood(){ return food; }
+
+    public Tracker getTracker(){ return tracker;}
 }
