a254d0f8e6807531555ceb9cbc6e67c0bb04be66
MJ Munkhbayar <78574063+Manal-jpg@users.noreply.github.com>
2023-12-04 01:27:07 -0500

    Added Food Controller Test

--- test/use_case/food/FoodControllerTest.java
+++ test/use_case/food/FoodControllerTest.java
@@ -6,7 +6,36 @@ import static org.junit.Assert.*;
 
 public class FoodControllerTest {
 
+    private FakeFoodInputBoundary interactor = new FakeFoodInputBoundary();
     @Test
     public void execute() {
+
+        //        Creating parameters for testing controller
+
+        String foodName = "apple";
+        Float quantity = 1.0f;
+
+        FoodController foodController = new FoodController(interactor);
+        foodController.execute(foodName, quantity);
+
+        //        Checking if the expected input data was used in execute
+        //
+        FoodInputData expected = new FoodInputData(foodName, quantity);
+        assertEquals(expected.getFoodName(), interactor.getLastExecutedFoodInputData().getFoodName());
+        assertEquals(expected.getFoodQuantity(), interactor.getLastExecutedFoodInputData().getFoodQuantity(), 0.001);
+
+    }
+
+    private static class FakeFoodInputBoundary implements FoodInputBoundary{
+
+        private FoodInputData lastExecutedInputData;
+        @Override
+        public void execute(FoodInputData foodInputData) {
+            lastExecutedInputData = foodInputData;
+        }
+
+        public FoodInputData getLastExecutedFoodInputData() {
+            return lastExecutedInputData;
+        }
     }
 }
