421e527fe3fdad8c7542adf9b890c8cadb84e432
MJ Munkhbayar <78574063+Manal-jpg@users.noreply.github.com>
2023-12-04 00:46:19 -0500

    Refactoring FoodTest

--- test/entity/FoodTest.java
+++ test/entity/FoodTest.java
@@ -6,31 +6,4 @@ import static org.junit.Assert.*;
 
 public class FoodTest {
 
-    @Test
-    public void getName() {
-    }
-
-    @Test
-    public void getCalories() {
-    }
-
-    @Test
-    public void updateQuantity() {
-    }
-
-    @Test
-    public void getNutritionalValues() {
-    }
-
-    @Test
-    public void getProtein() {
-    }
-
-    @Test
-    public void getFat() {
-    }
-
-    @Test
-    public void getCarbs() {
-    }
 }
