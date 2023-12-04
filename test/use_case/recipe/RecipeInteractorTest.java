27f73a3d514c412f35c93e4325ea5b81c842e956
Rachel Kim <rachelkim4321@gmail.com>
2023-12-04 00:59:49 -0500

    finished login controller and presetner tests and cleaned up recipe tests

--- test/use_case/recipe/RecipeInteractorTest.java
+++ test/use_case/recipe/RecipeInteractorTest.java
@@ -15,33 +15,6 @@ import java.util.Set;
 
 public class RecipeInteractorTest {
 
-//    private Recipe test;
-//    private RecipeDataAccessInterface fixedDAO;
-//    private RecipeDataAccessInterface testDAO;
-//    private RecipeInteractor interactor;
-
-//    @BeforeEach
-//    public void setUp(){
-//        fixedDAO = new RecipeDataAccessInterface() {
-//            @Override
-//            public Integer getFoodCalories(String foodName, Float quantity) {
-//                return 1465;
-//            }
-//
-//            @Override
-//            public HashMap<String, Double> getFoodNutritionalValues(String foodName, Float quantity) {
-//                HashMap<String, Double> nutritionalValues = new HashMap<>();
-//                nutritionalValues.put("Protein", 4.0);
-//                return nutritionalValues;
-//            }
-//        };
-//
-//
-//
-//        interactor = new RecipeInteractor(fixedDAO, outputBoundary);
-//
-//    }
-
     @Test
     public void testSuccess() {
         Food rice = new Food("rice", 1);
@@ -75,22 +48,3 @@ public class RecipeInteractorTest {
         interactor.execute(inputData);
     }
 }
-
-
-//    @Test
-//    public void execute() {
-//        Food rice = new Food("rice", 1);
-//        Food egg = new Food("egg", 1);
-//
-//        LinkedHashMap<Food, Integer> ingredients = new LinkedHashMap<Food, Integer>();
-//
-//        ingredients.put(rice, 2);
-//        ingredients.put(egg, 1);
-//
-//        RecipeInputData inputData = new RecipeInputData("egg rice", ingredients);
-//
-//        interactor.execute(inputData);
-//
-//
-//    }
-//}
